aa29d60fcb9c5db21be28bb33a96c6e7d7e88085
Ali MARZOUK <ali.marzouk@edifixio.com>
2024-12-09 13:15:06 +0100

    complete day 3

--- day_3.py
+++ day_3.py
@@ -6,26 +6,22 @@ import re
 def getMuliplicationResultPartOne(inputString: str) -> int:
     return sum(map(lambda x: x[0]*x[1], map(lambda x: eval(x), re.findall(r"mul\((\d+,\d+)\)", inputString))))
 
-def getMuliplicationResultPartTwo(inputString: str) -> int:
-    """
-    NOT WORKING
-    """
-    potential_found_list = re.findall(r".*?mul\(\d+,\d+\)", inputString)
-    print(len(potential_found_list))
-    print(potential_found_list)
-    def checkActivated(element: str) -> bool:
-        print('dont', re.search(r"don't\(\)(?!.*don't\(\))", element))
-        print('do', re.search(r"do\(\)(?!.*do\(\))", element))
-        if deactivatedMatch := re.search(r"don't\(\)(?!.*don't\(\))", element):
-            print('deactivated', deactivatedMatch)
-            if activatedMatch := re.search(r"do\(\)(?!.*do\(\))", element):
-                return activatedMatch > deactivatedMatch
-        return True
+def getMuliplicationResultPartTwo(inputString: str, active: bool = True) -> int:
+    do_match = re.search("do()", inputString)
+    do_position = do_match.start() if do_match else None
+    dont_match = re.search("don't()", inputString)
+    dont_position = dont_match.start() if dont_match else None
+    matches = re.finditer(r"mul\((\d+,\d+)\)", inputString, re.MULTILINE)
+    result = 0
+    for match in matches:
+        if dont_position != None and dont_position < match.start():
+            return result + getMuliplicationResultPartTwo(inputString[dont_position+5:], False)
+        if active or (do_position != None and match.start() > do_position):
+            numbers = eval(match.group(1))
+            result = result + (int(numbers[0]) * int(numbers[1]))
 
-    test_fn = lambda potential: eval(re.search(r"mul\((\d+,\d+)\)", potential).group(1))
-    return sum(map(lambda x: x[0]*x[1], map(test_fn, filter(checkActivated, potential_found_list))))
+    return result
+        
 
 print(getMuliplicationResultPartOne(input)) #174103751
-
-# print(getMuliplicationResultPartTwo("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")) #wrong 10959092 answer too low 
-# print(re.search(r"(?:do())?.*?mul\(\d+,\d+\)", ""))
+print(getMuliplicationResultPartTwo(input)) #100411201
